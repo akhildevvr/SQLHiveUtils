b350640d0aefe5cf4441cb89c20cffd7956d54fb
Akhil Ravindran <akhil.ravindran@autodesk.com>
2020-04-02 14:19:55 +0100

    Updated setup.py

--- SQLHiveUtils/setup.py
+++ SQLHiveUtils/setup.py
@@ -4,6 +4,7 @@ setup(
     name='SQLHiveUtils',
     version='0.0.1',
     description='My private package',
+    url='https://github.com/akhildevvr/SQLHiveUtils.git'
     license='unlicense',
     packages=['SQLHiveUtils'],
     zip_safe=False
